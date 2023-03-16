461cb0aada34404f3c9f54eb519d75247187ed06
rpinedab <richard.pineda@gmail.com>
2023-03-16 19:28:25 -0300

    commit produtcs

--- products/src/index.js
+++ products/src/index.js
@@ -1,15 +1,15 @@
-import express from 'express'   // const express = require("express")
+import express from 'express' // const express = require("express");
 import { randomBytes } from 'crypto'
 import bodyParser from 'body-parser'
 
 const app = express()
-app.use(bodyParser.json());
+app.use(bodyParser.json())
 
 const products = {}
 
 app.get('/products', (req, res) => {
-    res.send(products);
-});
+    res.send(products)
+})
 
 app.post('/products', (req, res) => {
     const id = randomBytes(4).toString('hex')
@@ -20,9 +20,9 @@ app.post('/products', (req, res) => {
         name
     }
 
-    req.status(201).send(products[id])
-});
+    res.status(201).send(products[id])
+})
 
 app.listen(3000, () => {
-    console.log("Listen on port 3000!");
-});
+    console.log("Listening on port 3000")
+})
